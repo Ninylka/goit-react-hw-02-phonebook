2f4722022144683c8e31f713cc296b5dec1dc116
Ninylka <shemetylka@gmail.com>
2023-11-06 14:44:35 +0100

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,94 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+
+
+import { Component } from 'react';
+import {
+  ContainerDivPhonebook,
+  TitlePhonebook,
+  FormPhonebook,
+  LabelPhonebook,
+  InputPhonebookName,
+  ButtonPhonebook
+} from './FormContacts/FormContacts.styled';
+import { nanoid } from 'nanoid';
+import { NumberPhone } from './NumberPhone/NumberPhone';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  
+  };
+
+  handleChange = (evt) => {
+    this.setState({ name: evt.target.value });
+  
+    
+  };
+
+
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+    const { name } = this.state;
+
+    const contact = {
+      id: nanoid(),
+      name,
+      
+    
+    };
+
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, contact],
+      name: '',
+    
+    }));
+  };
+
+
+
+  handleAddNumber = (number) => {
+    const { name } = this.state;
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+  
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, contact],
+      name: '',
+    }));
+  };
+
+
+  render() {
+    const { name, contacts } = this.state;
+    return (
+      <ContainerDivPhonebook>
+        <TitlePhonebook>Phonebook</TitlePhonebook>
+        <FormPhonebook onSubmit={this.handleSubmit}>
+          <LabelPhonebook>
+            Name
+            <InputPhonebookName
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.handleChange}
+              required
+            />
+          </LabelPhonebook>
+          <NumberPhone onAddNumber={this.handleAddNumber} />
+          <ButtonPhonebook type="submit">Add contact</ButtonPhonebook>
+        </FormPhonebook>
+        <h2>Contacts</h2>
+        <ul>
+          {contacts.map((contact) => (
+            <li key={contact.id}>{contact.name} : {contact.number}</li>
+          ))}
+        </ul>
+      </ContainerDivPhonebook>
+    );
+  }
+}
+
+
---
+++ src/components/FormContacts/FormContacts.styled.js
@@ -0,0 +1,51 @@
+
+import styled from 'styled-components'
+
+ 
+export const ContainerDivPhonebook = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+`
+export const TitlePhonebook = styled.h2`
+
+`
+export const FormPhonebook = styled.form`
+width: 300px;
+height: 250px;
+border: 2px solid #A3E4D7;
+border-radius: 10px;
+padding: 20px;
+`
+   export const LabelPhonebook = styled.label`
+
+   `
+
+export const InputPhonebookName = styled.input`
+   display: block;
+   margin-top: 10px;
+   margin-bottom: 50px;
+   border: 2px solid #A3E4D7;
+   border-radius: 10px;
+   height: 20px;
+   width: 300px;
+   `
+export const InputPhonebookNumber = styled.input`
+   display: block;
+   margin-top: 10px;
+   margin-bottom: 50px;
+   border: 2px solid #A3E4D7;
+   border-radius: 10px;
+   height: 20px;
+   width: 300px;
+   `
+
+
+export const ButtonPhonebook = styled.button`
+border: 2px solid #A3E4D7;
+border-radius: 10px;
+font-size: 16px;
+background-color: #fff;
+
+`
